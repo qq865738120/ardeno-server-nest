db38066f0a33e8aa2f6ffd24ee2330c9f54b7917
zwj <zwj865738120@163.com>
2021-11-11 10:19:25 +0800

    自动化部署

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,6 +19,33 @@ module.exports = {
       vizion: false,
       restart_delay: 60,
       max_memory_restart: '1G',
+      env: {
+        // 指定为生成环境
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'ardeno-server-test',
+      cwd: './',
+      script: 'yarn',
+      args: 'start:prod',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      error_file: './logs/app.error.log',
+      out_file: './logs/app.out.log',
+      instances: 1,
+      min_uptime: '120s',
+      max_restarts: 10,
+      cron_restart: '1 0 * * *',
+      merge_logs: true,
+      exec_interpreter: 'node',
+      exec_mode: 'fork',
+      autorestart: true,
+      vizion: false,
+      restart_delay: 60,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
     },
   ],
 
@@ -31,14 +58,9 @@ module.exports = {
       repo: 'git@github.com:qq865738120/ardeno-server-nest.git',
       path: '/root/projects/ardeno/server/production',
       ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
-      'post-deploy': 'npm install && yarn build && pm2 start --env production',
-      env: {
-        // 指定为生成环境
-        NODE_ENV: 'production',
-      },
+      'post-deploy': 'npm install && yarn build && pm2 start',
     },
     development: {
-      name: 'ardeno-server-develop',
       user: 'root',
       host: 'frp.ddnszwj.top',
       port: '6004',
@@ -46,10 +68,7 @@ module.exports = {
       repo: 'git@github.com:qq865738120/ardeno-server-nest.git',
       path: '/root/projects/ardeno/server/development',
       ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
-      'post-deploy': 'npm install && yarn build && pm2 start --env development',
-      env: {
-        NODE_ENV: 'development',
-      },
+      'post-deploy': 'npm install && yarn build && pm2 start',
     },
   },
 };

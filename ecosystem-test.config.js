9ddc38cc21826109fa8c9a64a8d52ffd6242def1
zwj <zwj865738120@163.com>
2021-11-11 10:42:49 +0800

    自动化部署

--- ecosystem-test.config.js
+++ ecosystem-test.config.js
@@ -34,7 +34,8 @@ module.exports = {
       repo: 'git@github.com:qq865738120/ardeno-server-nest.git',
       path: '/root/projects/ardeno/server/development',
       ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
-      'post-deploy': 'npm install && yarn build && pm2 start',
+      'post-deploy':
+        'npm install && yarn build && pm2 start --name ardeno-server-test',
     },
   },
 };

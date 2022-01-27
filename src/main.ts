edd0d0f943c1b5f95f92b29c76a2f5172d602356
zwj <zwj865738120@163.com>
2022-01-27 11:40:53 +0800

    feat:增加log

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ trustProxy: true }),
     {
       cors: true,
     },

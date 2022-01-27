93a28c08a5aab63bdd46c3f1db7233085fe59eb6
zwj <zwj865738120@163.com>
2022-01-27 11:24:49 +0800

    feat:增加log

--- src/common/decorator/ip-address-decorator.ts
+++ src/common/decorator/ip-address-decorator.ts
@@ -1,10 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, Logger } from '@nestjs/common';
 
 import * as requestIp from 'request-ip';
 
 export const IpAddress = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
+    Logger.log(req.headers, '---------req.headers-------');
+    Logger.log(req.clientIp, '---------req.clientIp-------');
     if (req.clientIp) return req.clientIp;
     return requestIp.getClientIp(req);
   },
